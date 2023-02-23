67040c378fa3f0799a2cedf6e46b78911009ad1a
SuperKXT <superkxt@outlook.com>
2023-02-23 16:17:42 +0500

    Complete api design

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Request, response, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 import { z } from 'zod';
 
@@ -7,22 +7,90 @@ const app = express();
 app.use(express.json());
 app.use(express.json());
 
+const errorCodes = {
+	BAD_INPUT: 1,
+	SERVER_ERROR: 2,
+	NOT_FOUND: 3,
+};
+
+const responses = {
+	BAD_INPUT: {
+		success: false,
+		errors: [errorCodes.BAD_INPUT],
+		data: null,
+	},
+	SERVER_ERROR: {
+		success: false,
+		errors: [errorCodes.SERVER_ERROR],
+		data: null,
+	},
+	NOT_FOUND: {
+		success: false,
+		errors: [errorCodes.NOT_FOUND],
+		data: null,
+	},
+	SUCCESS: (data: any) => ({
+		success: true,
+		errors: null,
+		data,
+	}),
+};
+
 app.get('/', async (_req: Request, res: Response) => {
-	res.json({ message: 'Hello from Express Prisma Boilerplate!' });
+	res.status(200).json(responses.SUCCESS('Hello!'));
+});
+
+const postSchema = z.strictObject({
+	title: z.string().max(100),
+	description: z.string().max(500),
+	questions: z.array(
+		z.strictObject({
+			description: z.string().max(500),
+			mandatory: z.boolean().default(false),
+			answers: z.array(
+				z.strictObject({
+					description: z.string().max(500),
+					correct: z.boolean().default(false),
+				})
+			),
+		})
+	),
 });
 
 app.post('/quiz', async (req: Request, res: Response) => {
 	try {
-		const user = await prisma.quiz.create({ data: req.body });
-		res.status(201).json({ message: 'success', data: user });
+		const parsed = postSchema.safeParse(req.body);
+		if (!parsed.success) return res.status(400).json(responses.BAD_INPUT);
+		const createData = {
+			...parsed.data,
+			questions: {
+				create: parsed.data.questions.map((row) => ({
+					...row,
+					answers: {
+						create: row.answers,
+					},
+				})),
+			},
+		};
+		const user = await prisma.quiz.create({
+			data: createData,
+			include: {
+				questions: {
+					include: {
+						answers: true,
+					},
+				},
+			},
+		});
+		res.status(201).json(responses.SUCCESS(user));
 	} catch (error: any) {
-		res.status(500).json({ message: error.message, data: null });
+		res.status(500).json(responses.SERVER_ERROR);
 	}
 });
 
 app.get('/quiz', async (_req: Request, res: Response) => {
 	try {
-		const quiz = await prisma.quiz.findMany({
+		const quizzes = await prisma.quiz.findMany({
 			include: {
 				questions: {
 					include: {
@@ -36,9 +104,9 @@ app.get('/quiz', async (_req: Request, res: Response) => {
 				},
 			},
 		});
-		res.status(200).json({ message: 'success', data: quiz });
+		res.status(200).json(responses.SUCCESS(quizzes));
 	} catch (error: any) {
-		res.status(500).json({ message: error.message, data: null });
+		res.status(500).json(responses.SERVER_ERROR);
 	}
 });
 
@@ -46,13 +114,23 @@ app.get('/quiz/:id', async (req: Request, res: Response) => {
 	try {
 		const quiz = await prisma.quiz.findUnique({
 			where: { id: Number(req.params.id) },
-			include: { questions: true },
+			include: {
+				questions: {
+					include: {
+						answers: {
+							select: {
+								id: true,
+								description: true,
+							},
+						},
+					},
+				},
+			},
 		});
-		if (!quiz)
-			return res.status(404).json({ message: 'not found!', data: null });
-		res.status(200).json({ message: 'success', data: quiz });
+		if (!quiz) return res.status(404).json(responses.NOT_FOUND);
+		res.status(200).json(responses.SUCCESS(quiz));
 	} catch (error: any) {
-		res.status(500).json({ message: error.message, data: null });
+		res.status(500).json(responses.SERVER_ERROR);
 	}
 });
 
@@ -66,31 +144,39 @@ const quizAttemptSchema = z.array(
 app.post('/quiz/:id', async (req: Request, res: Response) => {
 	try {
 		const parsed = quizAttemptSchema.safeParse(req.body);
-		if (!parsed.success)
-			return res
-				.status(400)
-				.json({ message: 'invalidInput', data: null });
+		if (!parsed.success) return res.status(400).json(responses.BAD_INPUT);
 		const { data } = parsed;
-		const questions = await prisma.question.findMany({
+		const quiz = await prisma.quiz.findUnique({
 			where: { id: Number(req.params.id) },
 			include: {
-				answers: true,
+				questions: {
+					include: {
+						answers: {
+							where: {
+								correct: true,
+							},
+						},
+					},
+				},
 			},
 		});
-		if (!questions.length)
-			return res.status(404).json({ message: 'not found!', data: null });
+		if (!quiz) return res.status(404).json(responses.NOT_FOUND);
 		let score = 0;
-		for (const { questionId, answerId } of data) {
-			const question = questions.find((row) => row.id === questionId);
-			if (question?.mandatory && !answerId)
-				return res.status(400).json({
-					message: 'not found!',
-					data: 'all mandatory questions not answered!',
-				});
+		for (const { answers, mandatory } of quiz.questions) {
+			const correct = answers[0];
+			if (!correct) return res.status(400).json(responses.BAD_INPUT);
+			const answer = data.find(
+				(row) => row.questionId === correct.questionId
+			)?.answerId;
+			if (mandatory && !answer)
+				return res.status(400).json(responses.BAD_INPUT);
+			if (correct.id === answer) score++;
 		}
-		res.status(200).json({ message: 'success', data: score });
+		res.status(200).json(
+			responses.SUCCESS({ score: `${score}/${quiz.questions.length}` })
+		);
 	} catch (error: any) {
-		res.status(500).json({ message: error.message, data: null });
+		res.status(500).json(responses.SERVER_ERROR);
 	}
 });
 
